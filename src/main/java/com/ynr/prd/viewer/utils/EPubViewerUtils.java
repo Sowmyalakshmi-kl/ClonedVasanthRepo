29b294e32a362fb1b5cff11819b55dc4e773c6d0
Vasanth <vasanthakumar>
2017-10-25 19:46:39 +0530

    Application Utils created for common methods and related functionality.

---
+++ src/main/java/com/ynr/prd/viewer/utils/EPubViewerUtils.java
@@ -0,0 +1,12 @@
+package com.ynr.prd.viewer.utils;
+
+import java.util.Date;
+
+public class EPubViewerUtils {
+
+	public static Date getCurrentTimestamp() {
+		
+			return new Date(System.currentTimeMillis());
+	}
+
+}
